ef444b50d63e0f760cf1f3df7c21605e85d50693
Lehan Coetzee <coetzeel@live.co.za>
2017-06-06 08:28:48 +0200

    Updated comments

--- scraper/views.py
+++ scraper/views.py
@@ -7,10 +7,16 @@ from pyramid.httpexceptions import HTTPBadRequest
 import validators
 from tld import get_tld
 
+# Serves the home page template, which is the user interface for the application
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def home(request):
     return {'project': 'scraper'}
 
+# The scraper route
+# Takes a json object with a wikipedia URL submitted via post
+# The URL will then be validated, if validation passes
+# The wikipedia page will be scraped for a Table of Contents and
+# Returned to the calling client
 @view_config(route_name='scraper', renderer='json')
 def scraper(request):
 	# Get the posted URL
@@ -31,11 +37,13 @@ def scraper(request):
     page = requests.get(url)
     # Intansiate beautiful soup with the html content
     soup = BeautifulSoup(page.content, 'html.parser')
-    # attenmpt to find a table of contents id
+    # Attenmpt to find a table of contents(toc) id
     toc = soup.find(id="toc")
 
+    # Check if a toc could be found
     if (str(toc) == "None"):
     	request.response.status = 400
     	return {'error': 'Table of Contents could not be found.'}     	
     else:	
+        # return the toc
         return Response(str(toc))
